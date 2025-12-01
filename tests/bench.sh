0e98daf5e2bb9f0428531c02fcd3d656400b2c51
admin_ <admin@chandlerding.altostrat.com>
2025-12-01 00:34:32 +0000

    tests/benchmark instructions

--- tests/bench.sh
+++ tests/bench.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-  
+# Run this script in the repo root directory, e.g. cd .. ; tests/bench.sh
+# make sure to build both the plugin and also configuration follow the guide in README.md before run this
+
 docker run -it -v $(pwd):/mnt  \
     us-docker.pkg.dev/service-extensions-samples/plugins/wasm-tester:main  \
-    --proto /mnt/tests.textpb   \
+    --proto /mnt/tests/tests.textpb   \
     --plugin /mnt/target/wasm32-wasip1/release/wasm_ip.wasm \
-    --config /mnt/plugin.config 
+    --config /mnt/tests/plugin.config 
